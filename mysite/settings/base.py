323c9217728ffba816c9b630b555cdc5aeb19da4
Dbonbon <yrfriedman@gmail.com>
2023-03-29 22:01:40 +0200

    second

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -87,7 +87,13 @@ WSGI_APPLICATION = 'mysite.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase', # This is where you put the name of the db file.
+                 # If one doesn't exist, it will be created at migration time.
+    }
+}
 
 DATABASES = {
     "default": {
